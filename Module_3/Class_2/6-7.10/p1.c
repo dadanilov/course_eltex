b49c543ceff66a29223d7b1928027d6f3dadea20
Danil <Danilov.Danil@outlook.com>
2021-11-09 14:01:11 +0700

    Задача 6-7.10 решена в модуле 3, в занятии 2

---
+++ Module_3/Class_2/6-7.10/p1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<errno.h>
+int main()
+{
+	int shmid, i = 0, len = 0;
+	char *array,pathname[]="result.c", a;
+	key_t key;
+	FILE *f = fopen("p1.c","r");
+	if ((key = ftok(pathname, 0)) < 0)
+	{
+		printf("Ошибка: не удается сгенерировать ключ.\n");
+	}
+	if ((shmid = shmget (key, 1280*sizeof (char), 0666 | IPC_CREAT|IPC_EXCL)) < 0)
+	{
+		if (errno != EEXIST)
+		{
+			printf("Ошибка: не удается создать общую память.\n");
+		}
+		else
+		{
+			if ((shmid = shmget (key, 1280*sizeof (char), 0)) < 0)
+			{
+				printf("Ошибка: не удается найти общую память.\n");
+			}
+		}
+	}
+	if ((array=(char *)shmat(shmid, NULL, 0)) == (char *)(-1))
+	{
+		printf("Ошибка: не удается подключить общую память.\n");
+	}
+	i=0;
+	while (fscanf(f,"%c",&a) == 1)
+	{
+		array[i]=a;
+		i++;
+	}
+	if (shmdt(array) < 0)
+	{
+		printf("Ошибка: не удается отсоединить общую память.\n");
+	}
+	pclose(f);
+	return 0;
+}
