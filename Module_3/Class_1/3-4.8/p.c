b1b37aff7b467dd09f13e002e6326223d95dd506
Danil <Danilov.Danil@outlook.com>
2021-11-12 19:09:35 +0700

    Задача 9.8 решена в модуле 3

--- Module_3/Class_1/3-4.8/p.c
+++ Module_3/Class_1/3-4.8/p.c
@@ -3,9 +3,12 @@
 
 int main()
 {
-	int pid, ppid;
+	pid_t pid, ppid, chpid;
+	int a = 0;
+	chpid = fork();
+	a = a+1; 
 	pid = getpid();
 	ppid = getppid();
-	printf("Мой ID = %d, ID родителя = %d\n", (int)pid, (int)ppid);
+	printf("Мой PID = %d, мой PPID = %d, результат %d\n", (int)pid, (int)ppid, a);
 	return 0;
 } 
