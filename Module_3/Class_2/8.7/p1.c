37386f1a9100abfb5ba390a07800b8bc06c51d4f
Danil <Danilov.Danil@outlook.com>
2021-11-14 16:57:08 +0700

    Задача 8.7 исправлена в модуле 3

--- Module_3/Class_2/8.7/p1.c
+++ Module_3/Class_2/8.7/p1.c
@@ -19,7 +19,12 @@ int main()
 		printf("Не могу сгенерировать ключ!\n");
 		exit(-1);
 	}
-	if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0)
+	if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+    	{
+        	printf("Не могу получить доступ по ключу\n");
+        	exit(-1);
+    	}
+	if ((shmid = shmget(key, 3 * sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0)
 	{
 		if (errno != EEXIST)
 		{
@@ -28,7 +33,7 @@ int main()
 		} 
 		else 
 		{
-			if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0)
+			if ((shmid = shmget(key, 3 * sizeof(int), 0)) < 0)
 			{
 				printf("Не могу найти общую память!\n");
 				exit(-1);
@@ -36,42 +41,7 @@ int main()
 			new = 0;
 		}
 	}
-	if ((semid = semget(key, 1, 0666 | IPC_CREAT|IPC_EXCL)) < 0)
-	{
-		if(errno != EEXIST)
-		{
-			printf("Не может быть полусредним!\n");
-			exit(-1);
-		} 
-		else 
-		{
-			if ((semid = semget(key, 1, 0)) < 0)
-			{
-				printf("Не могу найти sem\n");
-				exit(-1);
-			}
-		}
-	}
-	else
-	{
-		mybuf.sem_op = 1;
-		mybuf.sem_flg = 0;
-		mybuf.sem_num = 0;
-		if (semop(semid, &mybuf, 1) < 0)
-		{
-			printf("Не могу дождаться состояния!\n");
-			exit(-1);
-		}
-	}
-	mybuf.sem_op = -1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop(semid, &mybuf, 1) < 0)
-	{
-		printf("Не могу дождаться состояния!\n");
-		exit(-1);
-	}
-	if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1))
+	if ((array = (int *) shmat(shmid, NULL, 0)) == (int *)(-1))
 	{
 		printf("Не могу подключить общую память!\n");
 		exit(-1);
@@ -82,24 +52,44 @@ int main()
 		array[1] = 0;
 		array[2] = 1;
 	} 
-	else 
+	else
 	{
-		array[0] += 1;
-		for (i=0; i<1000000000L; i++);
-		array[2] += 1;
-	}
-	printf("Программа №1 была создана %d раз(а), программа №2 - %d раз(а), всего - %d раз(а).\n", array[0], array[1], array[2]);
+		if (array[0] + array[1] != array[2])
+		{
+			mybuf.sem_op = 1;
+			mybuf.sem_flg = 0;
+			mybuf.sem_num = 0;
+			if (semop(semid, &mybuf, 1) < 0)
+			{
+				printf("Не могу дождаться состояния!\n");
+				exit(-1);
+			}
+		}
+		else
+		{
+			semctl(semid, 0, IPC_RMID, 0);
+            		if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+            		{
+                		printf("Не может быть полусредним\n");
+                		exit(-1);
+            		}
+            	}
+            	array[0] += 1;
+		for (i = 0; i < 1000000000L; i++);
+            	mybuf.sem_op = 1;
+		mybuf.sem_flg = 0;
+		mybuf.sem_num = 0;
+            	if (semop(semid, &mybuf, 1) < 0)
+		{
+			printf("Не могу дождаться состояния!\n");
+			exit(-1);
+		}
+            	array[2] += 1;
+     	}
+     	printf("Программа №1 была создана %d раз(а), программа №2 - %d раз(а), всего - %d раз(а).\n", array[0], array[1], array[2]);	
 	if (shmdt(array) < 0)
 	{
 		printf("Не удается отсоединить общую память!\n");
-		exit(-1);
-	}
-	mybuf.sem_op = 1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop(semid, &mybuf, 1) < 0)
-	{
-		printf("Не могу дождаться состояния!\n");
 		exit(-1);
 	}
 	return 0;
--- Module_3/Class_2/8.7/p2.c
+++ Module_3/Class_2/8.7/p2.c
@@ -5,12 +5,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/shm.h>
+
 int main()
 {
 	int *array;
 	int shmid, semid;
 	int new = 1;
-	char pathname[] = "p2.c";
+	char pathname[] = "p1.c";
 	key_t key;
 	long i;
 	struct sembuf mybuf;
@@ -19,7 +20,12 @@ int main()
 		printf("Не могу сгенерировать ключ!\n");
 		exit(-1);
 	}
-	if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0)
+	if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+	{
+		printf("Не может быть полусредним\n");
+	    	exit(-1);
+	}
+	if ((shmid = shmget(key, 3*sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0)
 	{
 		if (errno != EEXIST)
 		{
@@ -28,7 +34,7 @@ int main()
 		} 
 		else 
 		{
-			if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0)
+			if ((shmid = shmget(key, 3 * sizeof(int), 0)) < 0)
 			{
 				printf("Не могу найти общую память!\n");
 				exit(-1);
@@ -36,42 +42,7 @@ int main()
 			new = 0;
 		}
 	}
-	if ((semid = semget(key, 1, 0666 | IPC_CREAT|IPC_EXCL)) < 0)
-	{
-		if (errno != EEXIST)
-		{
-			printf("Не может быть полусредним!\n");
-			exit(-1);
-		} 
-		else 
-		{
-			if ((semid = semget(key, 1, 0)) < 0)
-			{
-				printf("Не могу найти sem\n");
-				exit(-1);
-			}
-		}
-	}
-	else
-	{
-		mybuf.sem_op = 1;
-		mybuf.sem_flg = 0;
-		mybuf.sem_num = 0;
-		if (semop(semid, &mybuf, 1) < 0)
-		{
-			printf("Не могу дождаться состояния!\n");
-			exit(-1);
-		}
-	}
-	mybuf.sem_op = -1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop(semid, &mybuf, 1) < 0)
-	{
-		printf("Не могу дождаться состояния!\n");
-		exit(-1);
-	}
-	if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1))
+	if ((array = (int *) shmat(shmid, NULL, 0)) == (int *)(-1))
 	{
 		printf("Не могу подключить общую память!\n");
 		exit(-1);
@@ -82,24 +53,44 @@ int main()
 		array[1] = 1;
 		array[2] = 1;
 	} 
-	else 
+	else
 	{
-		array[1] += 1;
+		if (array[0] + array[1] != array[2])
+        	{
+			mybuf.sem_op = -1;
+			mybuf.sem_flg = 0;
+			mybuf.sem_num = 0;
+			if (semop(semid, &mybuf, 1) < 0)
+			{
+				printf("Не могу дождаться состояния!\n");
+				exit(-1);
+			}
+		}
+		else
+		{
+			semctl(semid, 0, IPC_RMID, 0);
+			if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+            		{
+                		printf("Не может быть полусредним\n");
+                		exit(-1);
+            		}
+            	}
+            	array[1] += 1;
 		for (i=0; i<1000000000L; i++);
+		mybuf.sem_op = 1;
+		mybuf.sem_flg = 0;
+		mybuf.sem_num = 0;
+		if (semop(semid, &mybuf, 1) < 0)
+		{
+			printf("Не могу дождаться состояния!\n");
+			exit(-1);
+		}
 		array[2] += 1;
 	}
 	printf("Программа №1 была создана %d раз(а), программа №2 - %d раз(а), всего - %d раз(а).\n", array[0], array[1], array[2]);
 	if (shmdt(array) < 0)
 	{
 		printf("Не удается отсоединить общую память!\n");
-		exit(-1);
-	}
-	mybuf.sem_op = 1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop(semid, &mybuf, 1) < 0)
-	{
-		printf("Не могу дождаться состояния!\n");
 		exit(-1);
 	}
 	return 0;
